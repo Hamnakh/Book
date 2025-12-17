81a08be129613ae79610d1bdce940f0d8abc8d6e
Hamna khan <hk220582@gmail.com>
2025-12-17 22:38:51 +0500

    Hackathon

---
+++ docusaurus_textbook/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\Users\\Hamna\\Desktop\\Hackhathon\\Physical-AI-Humanoid-Robotics-Book-\\docusaurus_textbook\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\Hamna\\Desktop\\Hackhathon\\Physical-AI-Humanoid-Robotics-Book-\\docusaurus_textbook\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\Hamna\\Desktop\\Hackhathon\\Physical-AI-Humanoid-Robotics-Book-\\docusaurus_textbook\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\Hamna\\Desktop\\Hackhathon\\Physical-AI-Humanoid-Robotics-Book-\\docusaurus_textbook\\src\\css\\custom.css"),
+];
---
+++ docusaurus_textbook/.docusaurus/routes.js
@@ -0,0 +1,175 @@
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+
+export default [
+  {
+    path: '/Physical-AI-Humanoid-Robotics-Book-/__docusaurus/debug',
+    component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/__docusaurus/debug', 'c63'),
+    exact: true
+  },
+  {
+    path: '/Physical-AI-Humanoid-Robotics-Book-/__docusaurus/debug/config',
+    component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/__docusaurus/debug/config', 'f48'),
+    exact: true
+  },
+  {
+    path: '/Physical-AI-Humanoid-Robotics-Book-/__docusaurus/debug/content',
+    component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/__docusaurus/debug/content', '8fc'),
+    exact: true
+  },
+  {
+    path: '/Physical-AI-Humanoid-Robotics-Book-/__docusaurus/debug/globalData',
+    component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/__docusaurus/debug/globalData', 'a9a'),
+    exact: true
+  },
+  {
+    path: '/Physical-AI-Humanoid-Robotics-Book-/__docusaurus/debug/metadata',
+    component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/__docusaurus/debug/metadata', 'b5a'),
+    exact: true
+  },
+  {
+    path: '/Physical-AI-Humanoid-Robotics-Book-/__docusaurus/debug/registry',
+    component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/__docusaurus/debug/registry', '07f'),
+    exact: true
+  },
+  {
+    path: '/Physical-AI-Humanoid-Robotics-Book-/__docusaurus/debug/routes',
+    component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/__docusaurus/debug/routes', '47f'),
+    exact: true
+  },
+  {
+    path: '/Physical-AI-Humanoid-Robotics-Book-/docs',
+    component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs', 'c82'),
+    routes: [
+      {
+        path: '/Physical-AI-Humanoid-Robotics-Book-/docs',
+        component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs', '487'),
+        routes: [
+          {
+            path: '/Physical-AI-Humanoid-Robotics-Book-/docs',
+            component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs', '0b4'),
+            routes: [
+              {
+                path: '/Physical-AI-Humanoid-Robotics-Book-/docs/advanced-ai-control/module-5-advanced-ai',
+                component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs/advanced-ai-control/module-5-advanced-ai', '73f'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/Physical-AI-Humanoid-Robotics-Book-/docs/appendix/glossary',
+                component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs/appendix/glossary', 'd62'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/Physical-AI-Humanoid-Robotics-Book-/docs/appendix/references',
+                component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs/appendix/references', 'ce5'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/Physical-AI-Humanoid-Robotics-Book-/docs/appendix/resources',
+                component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs/appendix/resources', 'd76'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/Physical-AI-Humanoid-Robotics-Book-/docs/hardware-basics/module-3-hardware',
+                component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs/hardware-basics/module-3-hardware', 'eac'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/Physical-AI-Humanoid-Robotics-Book-/docs/humanoid-design/module-6-humanoid-design',
+                component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs/humanoid-design/module-6-humanoid-design', '3fc'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/Physical-AI-Humanoid-Robotics-Book-/docs/intro',
+                component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs/intro', '684'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/Physical-AI-Humanoid-Robotics-Book-/docs/introduction/intro',
+                component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs/introduction/intro', '0fd'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/Physical-AI-Humanoid-Robotics-Book-/docs/ros2-foundations/module-1-ros2',
+                component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs/ros2-foundations/module-1-ros2', 'a6f'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/Physical-AI-Humanoid-Robotics-Book-/docs/ros2-foundations/ros2-hands-on',
+                component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs/ros2-foundations/ros2-hands-on', 'b71'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/Physical-AI-Humanoid-Robotics-Book-/docs/simulation/digital-twins',
+                component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs/simulation/digital-twins', 'c23'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/Physical-AI-Humanoid-Robotics-Book-/docs/simulation/gazebo-unity',
+                component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs/simulation/gazebo-unity', 'bdc'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/Physical-AI-Humanoid-Robotics-Book-/docs/simulation/module-2-simulation',
+                component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs/simulation/module-2-simulation', 'd4b'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/Physical-AI-Humanoid-Robotics-Book-/docs/vla-systems/module-4-vla-foundations',
+                component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs/vla-systems/module-4-vla-foundations', 'b7f'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/Physical-AI-Humanoid-Robotics-Book-/docs/vla-systems/vla-action',
+                component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs/vla-systems/vla-action', '0d6'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/Physical-AI-Humanoid-Robotics-Book-/docs/vla-systems/vla-hands-on-basic',
+                component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs/vla-systems/vla-hands-on-basic', '336'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/Physical-AI-Humanoid-Robotics-Book-/docs/vla-systems/vla-language',
+                component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs/vla-systems/vla-language', '190'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/Physical-AI-Humanoid-Robotics-Book-/docs/vla-systems/vla-vision',
+                component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/docs/vla-systems/vla-vision', '25d'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '/Physical-AI-Humanoid-Robotics-Book-/',
+    component: ComponentCreator('/Physical-AI-Humanoid-Robotics-Book-/', '4f2'),
+    exact: true
+  },
+  {
+    path: '*',
+    component: ComponentCreator('*'),
+  },
+];
